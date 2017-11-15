aeab7e8b5cb1a72fbc753c9da5455ed3f8cd5e93
Kuzmrom7 <kuzmrom7@gmail.com>
2017-11-16 02:03:53 +0300

    litle commit

--- pkg/storage/pgsql/user.go
+++ pkg/storage/pgsql/user.go
@@ -1,1 +1,20 @@
 package pgsql
+
+
+const (
+	sqlGetUsers = `
+		SELECT * FROM users
+	`
+
+	sqlCreqteUser = `
+		INSERT INTO users (username, email) VALUES ($1, $2)
+	`
+)
+
+type UserModel struct {
+
+}
+
+func GetUser(){
+
+}
