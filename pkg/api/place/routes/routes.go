eceff88ecffa2cdf91d7a5710ec12a5a08183a23
Geeezzy <Trakir4ik@gmail.com>
2017-12-22 16:18:05 +0300

    litle fix

--- pkg/api/place/routes/routes.go
+++ pkg/api/place/routes/routes.go
@@ -8,6 +8,6 @@ import (
 var Routes = []http.Route{
 	{Path: "/place", Method: http.MethodGet, Middleware: []http.Middleware{middleware.Authenticate}, Handler: GetPlace},
 	{Path: "/place", Method: http.MethodPost, Middleware: []http.Middleware{middleware.Authenticate}, Handler: PlaceCreate},
-	{Path: "/place/{place}", Method: http.MethodPost, Middleware: []http.Middleware{middleware.Authenticate}, Handler: PlaceUpdate},
+	{Path: "/place/{place}", Method: http.MethodPut, Middleware: []http.Middleware{middleware.Authenticate}, Handler: PlaceUpdate},
 	{Path: "/typeplace", Method: http.MethodGet, Handler: PlaceList},
 }
