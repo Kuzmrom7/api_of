12631ee35e204fdb2c616e0a21e8e59d1ddb0a0e
Kuzmrom7 <kuzmrom7@gmail.com>
2018-03-21 02:01:01 +0300

    Remove middle

--- pkg/api/dich/routes/routes.go
+++ pkg/api/dich/routes/routes.go
@@ -8,7 +8,7 @@ import (
 var Routes = []http.Route{
 	{Path: "/dish", Method: http.MethodPost, Middleware: []http.Middleware{middleware.Authenticate}, Handler: DishCreate},
 	{Path: "/dish", Method: http.MethodPut, Middleware: []http.Middleware{middleware.Authenticate}, Handler: DishUpdate},
-	{Path: "/dish/place/{place}", Method: http.MethodGet, Middleware: []http.Middleware{middleware.Authenticate}, Handler: DishList},
+	{Path: "/dish/place/{place}", Method: http.MethodGet, Handler: DishList},
 	{Path: "/dish/{dish}", Method: http.MethodDelete, Middleware: []http.Middleware{middleware.Authenticate}, Handler: DishRemove},
 	{Path: "/dish/{dish}", Method: http.MethodGet, Handler: DishGet},
 	{Path: "/type/dish", Method: http.MethodGet, Handler: TypeDishList},
